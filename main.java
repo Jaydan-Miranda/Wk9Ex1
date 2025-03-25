ea0d29abc817101916f065dfe29196519632487a
Jaydan-Miranda <miry08733@gmail.com>
2025-03-24 19:15:43 -0500

    push

--- main.java
+++ main.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
  class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);  //scanner to read input
 
         System.out.println("Interest Calculator Program");
 
-        // Get principal amount
+        // Ask principal
         System.out.print("What is principal amount of the loan in dollars? ");
         double principal = scanner.nextDouble();
 
-        // Get interest rate
+        // Ask interest rate
         System.out.print("What is interest rate (input 0.05 for 5%)? ");
         double rate = scanner.nextDouble();
 
-        // Get loan period in years
+        // Ask loan period in years
         System.out.print("What is period of loan in years? ");
         int time = scanner.nextInt();
 
